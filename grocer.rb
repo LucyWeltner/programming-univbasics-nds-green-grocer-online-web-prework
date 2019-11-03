2df4eb34a55d3e668c4e64ce5fe76a88e422069c
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-03 20:50:19 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -67,8 +67,11 @@ def apply_clearance(cart)
   i = 0 
   while i < cart.length do
     if cart[i][:clearance] == true
-      cart[i][:price] = .8
+      cart[i][:price] = .8*cart[i][:price]
+    end
     i += 1
+  end
+  cart
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
