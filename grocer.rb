ca4fe9adf58f47e14959581a375d4f7955ad5dd2
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-03 20:16:45 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -47,6 +47,9 @@ def apply_coupons(cart, coupons)
     while j < coupons.length do
       if cart[i][:item] == coupons[j][:item] && cart[i][:count] >= coupons[j][:num]
         cart << {:item => "#{cart[i][:item]} W/ COUPON", :count => coupons[j][:num], :clearance => cart[i][:clearance], :price => coupons[j][:cost]/coupons[j][:num]}
+        if cart[i][:count] == coupons[j][:num]
+          cart.delete_at[i]
+        end
         if cart[i][:count] > coupons[j][:num]
           cart[i][:count] = cart[i][:count] - coupons[j][:num]
         end
